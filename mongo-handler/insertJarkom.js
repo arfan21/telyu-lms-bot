5bae492f625863a5c0fbca52e4de7f48973efcd8
arfan21 <arfan.jadul10@gmail.com>
2021-03-31 11:18:58 +0700

    try to fix double notif jarkom 2

--- mongo-handler/insertJarkom.js
+++ mongo-handler/insertJarkom.js
@@ -4,9 +4,16 @@ module.exports = async () => {
     try {
         const dataJarkom = await getTugasJarkom();
         if (dataJarkom) {
-            await Jarkom.findOneAndUpdate(dataJarkom, dataJarkom, {
-                upsert: true,
-            });
+            await Jarkom.findOneAndUpdate(
+                {
+                    title: dataJarkom.title,
+                    link_halaman: dataJarkom.link_halaman,
+                },
+                dataJarkom,
+                {
+                    upsert: true,
+                }
+            );
             console.log("tugas lab inserted");
         }
     } catch (error) {
