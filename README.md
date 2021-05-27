c7aff156aef970674178330b37b2c9f8fd4a0acb
arfan21 <arfan.jadul10@gmail.com>
2021-05-28 05:57:47 +0700

    add .env.example and readme.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Discord bot Telyu LMS
+
+> discord bot to take assignments for courses in Telyu LMS
---
+++ mongoUri.example.js
@@ -0,0 +1,3 @@
+// URI mongodb
+module.exports =
+    "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]";
